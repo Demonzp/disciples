4a34f745f580b3a33d13d179460fee847c7a0658
DemoN <demon.ua.zp@gmail.com>
2023-06-12 16:57:24 +0300

    first commit

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,14 @@
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
+
+export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+            favicon: paths.favicon
+        }),
+        new webpack.ProgressPlugin(),
+    ]
+}
