29c44d49bd4e163a7dd128744816c7c061c8dd9e
DemoN <demon.ua.zp@gmail.com>
2024-04-16 20:19:21 +0300

    update

--- src/utils/game/objects/ArenaUnitPortrait.ts
+++ src/utils/game/objects/ArenaUnitPortrait.ts
@@ -1,27 +1,36 @@
 import { IUnit, portretPartyOneData } from "store/slices/sliceGame";
+import Container from "utils/gameLib/Container";
 import { TPoint } from "utils/gameLib/Game";
 import Scene from "utils/gameLib/Scene";
 import Sprite from "utils/gameLib/Sprite";
 
 const coordinats:{[key:string]:TPoint} = {
-    '00':{x:220,y:49},
-    '01':{x:303,y:49},
-    '10':{x:220,y:154},
-    '11':{x:303,y:154},
-    '20':{x:220,y:260},
-    '21':{x:303,y:260},
+    '00':{x:219,y:49},
+    '01':{x:301,y:49},
+    '10':{x:219,y:154},
+    '11':{x:301,y:154},
+    '20':{x:219,y:260},
+    '21':{x:301,y:260},
 };
 
 export default class ArenaUnitPortrait{
     private startPos:TPoint = {x:0,y:0};
     private sprite: Sprite;
+    private container: Container;
     constructor(private scene:Scene, public unit:IUnit){
-        this.startPos = coordinats[`${unit.position[0]}${unit.position[1]}`];
-        if(unit.numCells===2){
-            this.sprite = this.scene.add.sprite(`portret-units-two`,this.startPos.x,this.startPos.y);
+        this.create();
+    }
+
+    create(){
+        this.startPos = coordinats[`${this.unit.position[0]}${this.unit.position[1]}`];
+        this.container = this.scene.add.container(this.startPos.x,this.startPos.y);
+        if(this.unit.numCells===2){
+            this.container.x -= 40;
+            this.sprite = this.scene.add.sprite(`portret-units-two`);
         }else{
-            this.sprite = this.scene.add.sprite(`portret-units-one-${this.unit.fraction}`,this.startPos.x,this.startPos.y);
+            this.sprite = this.scene.add.sprite(`portret-units-one-${this.unit.fraction}`);
         }
         this.sprite.setFrame(portretPartyOneData[this.unit.icon]);
+        this.container.add([this.sprite]);
     }
 }
